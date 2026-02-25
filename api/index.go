df6d2481b01655a59de1dbd183cbe7e23e091bd4
juanq <juan.quintana@y.uno>
2026-02-25 09:53:01 -0500

    feat: add vercel config

---
+++ api/index.go
@@ -0,0 +1,17 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(map[string]any{
+		"status":    "ok",
+		"service":   "chargeback-intelligence-api",
+		"timestamp": time.Now().UTC().Format(time.RFC3339),
+	})
+}
